1811cbc57a163ba290f9058fc1467972652834d4
Jason Lee <huacnlee@gmail.com>
2019-01-07 13:07:31 +0800

    Remove Redis

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
 'use strict';
 
-const redis = require('redis');
 const process = require("process");
 const express = require('express');
 const bodyParser = require('body-parser');
 const mj = require("mathjax-node/lib/main.js");
 
 const port = 4010;
-const redisURL = process.env.REDIS_URL || 'redis://localhost:6379/1';
-const redisClient = redis.createClient(redisURL);
-const cacheTTL = 7 * 24 * 60 * 60;
 
 mj.config({  extensions: "TeX/color" });
 
 const app = express();
 app.use(bodyParser.text({extended: true}));
 
-const generateCacheKey = (key) => {
-  return "mathjax-v1-" + key;
-}
-
 app.get('/svg', (req, res) => {
   const tex = req.query.tex;
 
   console.log("GET /svg", tex)
   res.setHeader("content-type", "image/svg+xml");
-
-  const cacheKey = generateCacheKey(tex);
-  const cacheObj = redisClient.get(cacheKey);
-  if (cacheObj) {
-    console.log("cache hit", cacheKey);
-    res.send(cacheObj);
-    return;
-  }
 
   try {
     mj.typeset({
@@ -43,8 +27,6 @@ app.get('/svg', (req, res) => {
       svgNode: true,
       mml: true
     }, (result) => {
-      redisClient.setex(cacheKey, cacheTTL, result['svg']);
-      console.log("cache write", cacheKey);
       res.send(result["svg"]);
     });
   } catch (err) {
